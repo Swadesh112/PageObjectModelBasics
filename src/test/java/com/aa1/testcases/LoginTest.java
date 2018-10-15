5b055653a244864e48456ee45ad4b328b786deda
Swadesh Islam <swadesh.islam@lexmark.com>
2018-10-16 02:59:21 +0530

    Test CI Once More for Login Test

--- src/test/java/com/aa1/testcases/LoginTest.java
+++ src/test/java/com/aa1/testcases/LoginTest.java
@@ -11,7 +11,7 @@ import com.aa1.utilities.Utilities;
 
 public class LoginTest extends BaseTest{
 	
-	//-------------Test CI for Login Test Case-----------------------
+	//-------------Test CI Once More for Login Test Case-----------------------
 	@Test(dataProviderClass = Utilities.class, dataProvider ="dp")
 	public void loginTest(Hashtable<String, String> data){
 		
