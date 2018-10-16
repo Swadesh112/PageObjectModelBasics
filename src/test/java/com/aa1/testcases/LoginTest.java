d0780ea6721953ac557ad02bf247d950145a9c0f
sislam <sislam@SISLAMWIN7A.na.ds.lexmark.com>
2018-10-16 14:04:34 +0530

    Commit for EGit Plugin

--- src/test/java/com/aa1/testcases/LoginTest.java
+++ src/test/java/com/aa1/testcases/LoginTest.java
@@ -11,7 +11,7 @@ import com.aa1.utilities.Utilities;
 
 public class LoginTest extends BaseTest{
 	
-	//-------------Test CI Once More for Login Test Case-----------------------
+	//-------------Test EGit Plugin for Login Test Case-----------------------
 	@Test(dataProviderClass = Utilities.class, dataProvider ="dp")
 	public void loginTest(Hashtable<String, String> data){
 		
